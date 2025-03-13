a3db0654ceb511429f115684f68519842329b62f
Suzanne <suzanne.chung@mohawkcollege.ca>
2025-03-12 21:55:53 -0400

    Get the Asteroids - loaded images into git

---
+++ chung-suzanne-a3-project/Game.cs
@@ -0,0 +1,38 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.IO;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D;
+
+/// <summary>
+///     Your game code goes inside this class!
+/// </summary>
+public class Game
+{
+    // Place your variables here:
+    const string assetFolder = "../../../../assets/";
+    // Load graphic
+    Texture2D texture = Graphics.LoadTexture(assetFolder + "mediAsteroids.png");
+
+    /// <summary>
+    ///     Setup runs once before the game loop begins.
+    /// </summary>
+    public void Setup()
+    {
+        Window.SetTitle("Get the Asteroids!");
+        Window.SetSize(500, 500);
+
+    }
+
+    /// <summary>
+    ///     Update runs every frame.
+    /// </summary>
+    public void Update()
+    {
+        // Prepare for drawing
+        Window.ClearBackground(Color.OffWhite);
+        Graphics.Draw(texture, 100, 100);
+    }
+}
