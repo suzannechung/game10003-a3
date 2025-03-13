fb83a6b58ba473dfc668c6298d461813ecbb9249
Suzanne <suzanne.chung@mohawkcollege.ca>
2025-03-12 23:01:03 -0400

    testing flying asteroids class

--- chung-suzanne-a3-project/Game.cs
+++ chung-suzanne-a3-project/Game.cs
@@ -16,6 +16,13 @@ public class Game
     // Load graphic
     Texture2D texture = Graphics.LoadTexture(assetFolder + "mediAsteroids.png");
 
+    Asteroids[] asteros = [new Asteroids(new Vector2(300, 200)), new Asteroids(new Vector2(100, 200)), new Asteroids(new Vector2(200, 200))];
+    int activeasteros = 0;
+    int countBulletHitEdges;
+    bool isGameOver;
+    int maxBulletCount = 1000;
+    int hitsRequired;
+
     /// <summary>
     ///     Setup runs once before the game loop begins.
     /// </summary>
@@ -32,7 +39,37 @@ public class Game
     public void Update()
     {
         // Prepare for drawing
+        //Window.ClearBackground(Color.OffWhite);
+        //Graphics.Draw(texture, 100, 100);
+        for (int i = 0; i < asteros.Length; i++)
+        {
+            asteros[i].Render();
+   
+        }
+
+    }
+
+    public void PlayGame()
+    {
         Window.ClearBackground(Color.OffWhite);
-        Graphics.Draw(texture, 100, 100);
+
+        //player.Update();
+
+        for (int i = 0; i < activeasteros; i++)
+        {
+            Asteroids bullet = asteros[i];
+            bullet.Update();
+            bullet.Render();
+            activeasteros++;
+
+            // does bullet hit player
+            //float distance = Vector2.Distance(player.position, bullet.GetPosition());
+            //if (distance < player.drawSize)
+            //{
+            //isGameOver = true;
+            //}
+        }
+
+        //player.Render();
     }
 }
---
+++ chung-suzanne-a3-project/flyingasteroids.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Numerics;
+
+namespace MohawkGame2D
+{
+    public class Asteroids
+    {
+        Vector2 position;
+        Vector2 direction;
+        float speed = 200;
+        float radius;
+        Color color;
+        bool hasHitScreenEdge;
+
+
+        public Asteroids(Vector2 pos)
+        {
+            position = pos;
+
+            // Give bullet a random direction and fixed speed
+            direction = Random.Direction();
+        }
+
+        public void Render()
+        {
+            const string assetFolder = "../../../../assets/";
+            Texture2D texture = Graphics.LoadTexture(assetFolder + "mediAsteroids.png");
+            Graphics.Draw(texture, position);
+        }
+
+        public void Update()
+        {
+            // add to speed
+            speed += Time.DeltaTime * 5;
+
+            // Add velocity to position, scaled by time
+            position += direction * speed * Time.DeltaTime;
+
+        }
+
+        public bool HasHitScreenEdge()
+        {
+            bool hitScreenEdge = hasHitScreenEdge;
+            hasHitScreenEdge = false;
+            return hitScreenEdge;
+        }
+
+        public Asteroids CreateClone()
+        {
+            Asteroids clone = new Asteroids(position);
+
+            // Add random deviation
+            Vector2 deviation1 = Random.Direction() / 3;
+            Vector2 deviation2 = Random.Direction() / 3;
+            // Reconstruct direction and reapply speed
+            this.direction = Vector2.Normalize(direction + deviation1);
+            clone.direction = Vector2.Normalize(direction + deviation2);
+
+            return clone;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return position;
+        }
+    }
+}
